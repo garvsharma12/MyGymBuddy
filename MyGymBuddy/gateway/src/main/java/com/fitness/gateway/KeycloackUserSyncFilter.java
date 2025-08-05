febdea9984025206605445c8c9bc3afe6f2e51dd
garvsharma12 <garv.sharma200212@gmail.com>
2025-08-05 20:37:43 +0530

    Synchronizing Keycloack User

---
+++ MyGymBuddy/gateway/src/main/java/com/fitness/gateway/KeycloackUserSyncFilter.java
@@ -0,0 +1,74 @@
+package com.fitness.gateway;
+
+import com.fitness.gateway.user.RegisterRequest;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import com.fitness.gateway.user.UserService;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class KeycloackUserSyncFilter implements WebFilter {
+
+    private final UserService userService;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        String userId = exchange.getRequest().getHeaders().getFirst("X-User-ID");
+        String token = exchange.getRequest().getHeaders().getFirst("Authorization");
+
+        if (userId != null && token != null) {
+            return userService.validateUser(userId)
+                    .flatMap(exist -> {
+                        if (!exist) {
+                            RegisterRequest registerRequest = getUserDetails(token);
+                            if (registerRequest != null) {
+                                return userService.registerUser(registerRequest)
+                                        .then(Mono.just(false)); // mark as registered
+                            } else {
+                                return Mono.just(false);
+                            }
+                        } else {
+                            log.info("User already exists: {}", userId);
+                            return Mono.just(true); // already existed
+                        }
+                    })
+                    .flatMap(ignored -> {
+                        ServerHttpRequest mutatedRequest = exchange.getRequest().mutate()
+                                .header("X-User-ID", userId)
+                                .build();
+                        return chain.filter(exchange.mutate().request(mutatedRequest).build());
+                    });
+        }
+
+        // if header not found, proceed without modification
+        return chain.filter(exchange);
+    }
+
+    private RegisterRequest getUserDetails(String token) {
+        try {
+            String tokenWithoutBearer = token.replace("Bearer", "").trim();
+            SignedJWT signedJWT = SignedJWT.parse(tokenWithoutBearer);
+            JWTClaimsSet claims = signedJWT.getJWTClaimsSet();
+
+            RegisterRequest registerRequest = new RegisterRequest();
+            registerRequest.setEmail(claims.getStringClaim("email"));
+            registerRequest.setKeyCloakId(claims.getStringClaim("sub"));
+            registerRequest.setPassword("dummy123");
+            registerRequest.setFirstName(claims.getStringClaim("given_name"));
+            registerRequest.setLastName(claims.getStringClaim("family_name"));
+            return registerRequest;
+        } catch (Exception e) {
+            log.error("Failed to parse token for registration: {}", e.getMessage());
+            return null;
+        }
+    }
+}
---
+++ MyGymBuddy/gateway/src/main/java/com/fitness/gateway/user/RegisterRequest.java
@@ -0,0 +1,23 @@
+package com.fitness.gateway.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid Email format")
+    private String email;
+
+    @NotBlank(message = "Password is Required")
+    @Size(min=6, message = "password must be of atleast 6 characters")
+    private String password;
+
+    private String keyCloakId;
+
+    private String firstName;
+    private String lastName;
+
+}
